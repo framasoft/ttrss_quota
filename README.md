a96e57ca8fdc12434be0735f0d34fd8f49d493dc
Luc Didry <luc@framasoft.org>
2013-08-25 14:56:51 +0200

    Working ! :D

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-framanews_quota
-===============
+Quota
+=====
 
-Plugin de quota pour Framanews (Tiny Tiny RSS)
+Quota plugin for Framanews (Tiny Tiny RSS)
+https://github.com/framasoft/framanews_quota.git
+
+Licensed under GNU GPL version 2
+
+Copyright (c) 2013 Luc Didry
+
+## Requirements
+
+* tt-rss v1.9 or be working from trunk at:
+https://github.com/gothfox/Tiny-Tiny-RSS
+
+##Install:
+Go to your ttrss dir and:
+```shell
+cd plugins
+git clone https://github.com/framasoft/framanews_quota.git
+```
+
+Enable plugin in config.php and put:
+
+```php
+define('FEED_QUOTA', 100);
+```
+
+in your config.php;
---
+++ init.php
@@ -0,0 +1,53 @@
+<?php
+class Quota extends Plugin {
+
+    private $link;
+    private $host;
+
+    function about() {
+        return array(1.0,
+            "Feeds quota system. Redirect to prefs with message if over quota",
+            "ldidry", true);
+    }
+
+    function init($host) {
+        $this->dbh = $host->get_dbh();
+        $this->host = $host;
+
+        $host->add_hook($host::HOOK_TOOLBAR_BUTTON, $this);
+    }
+
+    function HOOK_TOOLBAR_BUTTON() {
+        if ($this->over_quota() > 0) {
+            print '<script>window.location.href = "prefs.php";</script>';
+        }
+    }
+
+    function get_prefs_js() {
+        $over = $this->over_quota();
+        if ($over > 0) {
+            $title   = 'Trop de flux !';
+            $content = "Vous avez dépassé la limite de ".FEED_QUOTA." flux RSS.\\nVeuillez supprimer au moins $over flux pour continuer à utiliser Framanews.";
+            return "alert('$title\\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\\n$content');";
+        } else {
+            return '';
+        }
+    }
+
+    function over_quota() {
+        if(defined('FEED_QUOTA') && FEED_QUOTA > 0) {
+            $result = db_query( "SELECT COUNT(*) AS cf FROM ttrss_feeds WHERE owner_uid = ".$_SESSION['uid'].";");
+            $num_feeds = db_fetch_result($result, 0, "cf");
+
+            if ($num_feeds > FEED_QUOTA) {
+                return $num_feeds - FEED_QUOTA;
+            }
+        }
+        return 0;
+    }
+
+    function api_version() {
+        return 2;
+    }
+}
+?>
